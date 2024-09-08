096a2a8b9db17244378e3df8318c551da6d13bd1
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-09-08 13:38:40 -0400

    Basic Borrow Checker

--- rust_compiler/semantic/borrow_checker.go
+++ rust_compiler/semantic/borrow_checker.go
@@ -1,1 +1,45 @@
 package semantic
+
+import (
+	"fmt"
+	"rust_compiler/ast"
+)
+
+type BorrowChecker struct {
+	ownership map[string]string
+	errors    []string
+}
+
+func NewBorrowChecker() *BorrowChecker {
+	return &BorrowChecker{ownership: make(map[string]string), errors: []string{}}
+}
+
+func (bc *BorrowChecker) Check(program *ast.Program) {
+	for _, stmt := range program.Statements {
+		bc.checkStatement(stmt)
+	}
+}
+
+func (bc *BorrowChecker) checkStatement(stmt ast.Statement) {
+	switch stmt := stmt.(type) {
+	case *ast.LetStatement:
+		bc.checkLetStatement(stmt)
+	default:
+	}
+}
+
+func (bc *BorrowChecker) checkLetStatement(stmt *ast.LetStatement) {
+	if stmt.Value == nil {
+		return
+	}
+	if _, ok := stmt.Value.(*ast.Identifier); ok {
+		if bc.ownership[stmt.Name.Value] == "mut_borrowed" {
+			bc.errors = append(bc.errors, fmt.Sprintf("cannot borrow %s as immutable because it is already borrowed as mutable", stmt.Name.Value))
+		}
+		bc.ownership[stmt.Name.Value] = "borrowed"
+	}
+}
+
+func (bc *BorrowChecker) Errors() []string {
+	return bc.errors
+}
