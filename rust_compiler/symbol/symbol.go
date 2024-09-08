a12aba8454bcbb180396cfbd3034be7fe527ba04
Gfecito <71032552+Gfecito@users.noreply.github.com>
2024-09-08 13:38:30 -0400

    Basic Symbols and Scopes

--- rust_compiler/symbol/symbol.go
+++ rust_compiler/symbol/symbol.go
@@ -1,5 +1,7 @@
 package symbol
 
+import "errors"
+
 type Symbol interface {
 }
 
@@ -16,12 +18,36 @@ var (
 )
 
 type SymbolTable struct {
-	symbols map[string]Symbol
+	symbols map[string]*Symbol
+	// Parent scope.
+	outer *SymbolTable
+}
+
+// SYMBOL MANAGEMENT
+func (st *SymbolTable) Insert(name string, sym *Symbol) {
+	st.symbols[name] = sym
 }
 
 func (st *SymbolTable) Lookup(name string) Symbol {
 	if symbol, ok := st.symbols[name]; ok {
 		return symbol
 	}
-	return UndefinedSymbol
+
+	if st.outer == nil {
+		return UndefinedSymbol
+	}
+	return st.outer.Lookup(name)
+}
+
+// SCOPE MANAGEMENT
+func (st *SymbolTable) EnterScope() *SymbolTable {
+	// Make new inner scope
+	return &SymbolTable{symbols: make(map[string]*Symbol), outer: st}
+}
+
+func (st *SymbolTable) LeaveScope() (*SymbolTable, error) {
+	if st.outer == nil {
+		return nil, errors.New("Tried to leave scope but no outer scope was found.")
+	}
+	return st.outer, nil
 }
